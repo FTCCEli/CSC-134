e9a1dc66c2850412742fc2da10cdf2cb2173926f
FTCCEli <elimaximus11@gmail.com>
2025-02-09 20:02:14 +0000

    M1T2 started

---
+++ M2T1_Pabon.cpp
@@ -0,0 +1,15 @@
+// CSC 134
+// M2T1
+// Elijah Pabon
+// 2/9/2025
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    cout << "hello" << endl;
+    cout << "world";
+    return 0;
+
+}
