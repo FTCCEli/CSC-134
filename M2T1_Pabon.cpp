cd5a8e835449af276665a0c723b1e021970881a1
FTCCEli <elimaximus11@gmail.com>
2025-02-09 21:43:47 +0000

    finished

--- M2T1_Pabon.cpp
+++ M2T1_Pabon.cpp
@@ -10,42 +10,21 @@ using namespace std;
 
 int main() 
 {
-  // Apple sales program 
-  // We learnig about variables today
-  string farm_name = "Juicy's ";
-  int num_apples = 100; // "int" only represents whole nubers between -2,147,483,648 and 2,147,483,647
-  double cost_each = 0.25; // "double" can represnt decimals 
-  // New variables for M2T1
+  string name = "Elijah's"; 
+  int num_apples = 10; // "int" are whole number 
+  double price_each = 0.25; // "double" are decimals 
 
-  string user_name; // Who's buying Juicy's apples?
-  int apples_to_buy; // How many do they want.
+cout << "Welcome to the " << name << " apple farm!" << endl;
+cout << "We have " << num_apples << " in stock." << endl;
+cout << "Each apple cost $" << price_each << "!" << endl;
 
-  // greeting the customer
-  cout << "Howdy! What's your name? ";
-  cin >> user_name;
-  cout << "thanks for stopping by, " << user_name << "!" << endl;
+// Toltal price for all the apples to two decimal places
+cout << setprecision(2) << fixed;
+double total_price = num_apples * price_each; 
 
-  cout << "Welcome to " << farm_name << "apple farm!" << endl; 
-  cout << "There are " << num_apples << " apples in stock." << endl;
-  cout << "They cost $" << cost_each << " each." << endl;
-  
-  // This line sets cout for doubles to 2 decimal places
-  // you must put the line #include <iomanip> with the other #include statements up
-  cout << setprecision(2) << fixed;
-
-  // Compute total price 
-  double total_cost = num_apples * cost_each;
-  cout << "The total for all of " << farm_name << "apples is: $" << total_cost << endl;
-
-  // Closing the sale
-  cout << "How many apples would you like? "; 
-  cin >> apples_to_buy;
-
-  total_cost = apples_to_buy * cost_each;
-
-  cout << "Thank you! " << apples_to_buy << " apples comes to $" << total_cost << endl;
-  cout << "Have a nice day!" << endl;
-  cout << endl;
-
+// print totoal for all the apples
+cout << "The totall price for all " << num_apples;
+cout << " is: $" << total_price << endl;
+cout << "Thanksfor stopping by!" << endl;
     return 0; // no errors
 }
