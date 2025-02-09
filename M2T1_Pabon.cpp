2f9798b43881729caa2ad1a8622bf43ad398cc5f
FTCCEli <elimaximus11@gmail.com>
2025-02-09 20:51:49 +0000

    in progress

--- M2T1_Pabon.cpp
+++ M2T1_Pabon.cpp
@@ -4,12 +4,32 @@
 // 2/9/2025
 
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 int main() 
-{ 
-    cout << "hello" << endl;
-    cout << "world";
-    return 0;
+{
+  // Apple sales program 
+  // We learnig about variables today
+  string farm_name = "Juicy's";
+  int num_apples = 100; // "int" only represents whole nubers between -2,147,483,648 and 2,147,483,647
+  double cost_each = 0.25; // "double" can represnt decimals 
+  // New variables for M2T1
 
+  string user_name; // Who's buying Juicy's apples?
+  int apples_to_buy; // How many do they want.
+
+  // greeting the customer
+  cout << "Howdy! What's your name? ";
+  cin >> user_name;
+  cout << "thanks for stopping by, " << user_name << "!" << endl;
+
+  cout << "Welcome to " << farm_name << "apple farm!" << endl; 
+  cout << "There are " << num_apples << " apples oinn stock." << endl;
+  cout << they cou 
+
+
+
+    return 0; // no errors
 }
