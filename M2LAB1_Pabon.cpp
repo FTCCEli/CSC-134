8e99d3515d09980c7ddb62e056188ebfb5146286
FTCCEli <elimaximus11@gmail.com>
2025-02-14 23:34:34 +0000

    M2LAB1 Started

---
+++ M2LAB1_Pabon.cpp
@@ -0,0 +1,45 @@
+// CSC 134
+// M2LAB1
+// Elijah Pabon
+// 2/14/25
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+    // Constants for cost and amount
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double PRICE_PER_CUBIC_FOOT = 0.5;
+
+    // Variables 
+    double length,
+            width,
+            height,
+            volume,
+            cost,
+            charge,
+            profit;
+
+    // Set the desired output formatting for numbers.
+    cout << setprecision(2) << fixed << showpoint;
+
+    // Acquiring box dimensiont 
+    cout << "Enter the dimensions of the crate (in feet). " endl;
+    cout << "Length: ";
+    cin >> length;
+    cout << "Width: ";
+    cin >> width;
+    cout << "Height: ":
+    cin >> height;
+
+    // Computation of the crates volume, cost, price, and profit.
+
+    
+
+
+
+
+}
