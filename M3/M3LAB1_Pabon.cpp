752a4615e2d330229d186abd6a146298f29ec577
FTCCEli <elimaximus11@gmail.com>
2025-02-28 21:52:29 +0000

    in progress

---
+++ M3/M3LAB1_Pabon.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M3T2
+// Elijah Pabon
+// 2/22/2025
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+    int choice_1,
+        choice_2;
+
+    cout << "You're on a bridge that passes over a traintrack."<< endl;
+    cout << "You notice a train is on a sure path to kill 5 people working on a train track" << endl;
+    cout << "You calculate that if you push a the large gentleman next to you off the bridge you could "<< endl;
+    cout << "prevnt the train from killing the five workers."<< endl;
+    cout << "1: Push" << endl;
+    cout << "2: Pon't push" << endl;
+    
+    cin >> choice_1;
+
+    if ( choice_1 == 1)
+        {
+            cout << "The large mans body stops the train saving the workers." << endl;
+            cout << "No one is aware you pushed him." << endl;
+            
+        }
+
+ 
+}
