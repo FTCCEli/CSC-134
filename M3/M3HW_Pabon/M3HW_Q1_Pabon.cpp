fc7c98adeb9fd99c3d0f6bfee27be6acfb51a044
FTCCEli <elimaximus11@gmail.com>
2025-03-07 21:20:21 +0000

    in progress

---
+++ M3/M3HW_Pabon/M3HW_Q1_Pabon.cpp
@@ -0,0 +1,27 @@
+// CSC 134
+// M2LAB1
+// Elijah Pabon
+// 2/14/25
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+string answer = answer;
+    cout << "Hey, I'm a C++ chatbot." << endl;
+    cout << "Is your refrigerator running?" << endl;
+    cin >> answer;
+    if (answer == "yes"){
+        cout << "Then you better go catch it. jajajajaja.";
+    }
+    else if ( answer == "no"){
+        cout << "Then you should fix it. jajajajaja.";
+    }
+    else { 
+        cout << "invalide entry";
+    }
+
+    
+}
