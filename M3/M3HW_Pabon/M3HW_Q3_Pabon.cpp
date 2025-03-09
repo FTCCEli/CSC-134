bbb7ef1770495028980ef7b867ec811e9fd0e4bd
FTCCEli <elimaximus11@gmail.com>
2025-03-09 16:22:18 +0000

    working

--- M3/M3HW_Pabon/M3HW_Q3_Pabon.cpp
+++ M3/M3HW_Pabon/M3HW_Q3_Pabon.cpp
@@ -36,6 +36,7 @@ int main()
                 cout << "You run full speed at the fingure."<< endl;
                 cout << "Right as you make contact with figure your body jolts awake."<< endl;
                 cout << "It was just a dream."<< endl;
+                cout << endl;
             }
             else if ( choice_2 == 2)
             {
@@ -45,26 +46,52 @@ int main()
                 cout << "but you realize don't recognize any of the streets."<< endl;
                 cout << "You drive and drive to no end."<< endl;
                 cout << "The houses lining the street seem to go on endlessly."<< endl;
-                cout << "."<< endl;
-                
+                cout << "The houses look less and less livable, not rundown,"<< endl;
+                cout << "but built as if there not made to be lived in."<< endl;
+                cout << "Houses with no windows, some with driveways that don't"<< endl;
+                cout << "lead to street instead they just circle the house,"<< endl;
+                cout << "others with entraces at the second floor of the house."<< endl;
+                cout << "You continue driving until you see a house with it's"<< endl;
+                cout << "light on."<< endl;
+                cout << "Cautiously, you walk in the house."<< endl;
+                cout << "The house looks exactily like yours."<< endl;
+                cout << "You search the house but see no one."<< endl;
+                cout << "As you stand at the end of the hallway you hear the"<< endl;
+                cout << "phone ring."<< endl;
+                cout << "You see a some that looks identical to you walk to"<< endl;
+                cout << "and pick up the phone."<< endl;
+                cout << "They turn to look at you..."<< endl;
+                cout << "Their face looks scared and hepless."<< endl;
+                cout << "Before you can say anything they run out the door."<< endl;
+                cout << endl;
             }
             else 
             {
-                cout << "Your silence raises suspicion."<< endl;
-                cout << "You're taken in for questioning and later arrested for the homicide of Mr. Large Man." << endl;
+                cout << "You stand frozen."<< endl;
+                cout << "The figure approaches you and before you can react" << endl;
+                cout << "you succumb to the darkness."<< endl;
+                cout << endl;
             }
 
         }
 
     else if (choice_1 == 2)
         {
-            cout << "5 people died on the train tracks that day." << endl;
+            cout << "You wait for the ringing to stop." << endl;
+            cout << "It doesn't."<< endl;
+            cout << "It gets louder"<< endl;
+            cout << "The tones in the ring start merging together"<< endl;
+            cout << "and destorting."<< endl;
+            cout << "As the noise grows loader you sudenly feel weak."<< endl;
+            cout << "Houses with no windows, some with driveways that don't"<< endl;
+            cout << "Houses with no windows, some with driveways that don't"<< endl;
+            
         }
 
     else 
         {
             cout << "You reacted too slow." << endl;
-            cout << "5 people died on the train tracks that day." << endl;
+            cout << "Houses with no windows, some with driveways that don't"<< endl;
         }
     
     cout << "Game Over"<< endl;
