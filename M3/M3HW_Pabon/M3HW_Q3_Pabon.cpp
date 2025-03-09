d9ceb3e3628af5b7258e8ac1d3f0daadda11f9a5
FTCCEli <elimaximus11@gmail.com>
2025-03-09 17:03:52 +0000

    Finish Q3

--- M3/M3HW_Pabon/M3HW_Q3_Pabon.cpp
+++ M3/M3HW_Pabon/M3HW_Q3_Pabon.cpp
@@ -17,24 +17,24 @@ int main()
     cout << "You hear the phone ring down the hall." << endl;
     cout << "You don't recognize the phone number."<< endl;
     cout << "1: Answer the phone." << endl;
-    cout << "2: Don't answer teh phone." << endl;
+    cout << "2: Ignore phone." << endl;
     cout << "Enter the # that corresponds with your choice:";
 
     cin >> choice_1;
 
     if ( choice_1 == 1)
         {
-            cout << "The voice on the other side of the lines says \"Look behind you.\"" << endl;
-            cout << "You see tall masked figure standing at the end of the hallway." << endl;
+            cout << "The voice on the other side of the line says, \"Look behind you.\"" << endl;
+            cout << "You see a tall masked figure standing at the end of your hallway." << endl;
             cout << "1: Fight."<< endl;
             cout << "2: Run."<< endl;
-            cout << "Enter the # that corresponds with choice:";
+            cout << "Enter the # that corresponds with your choice:";
             cin >> choice_2;
 
             if ( choice_2 == 1)
             { 
-                cout << "You run full speed at the fingure."<< endl;
-                cout << "Right as you make contact with figure your body jolts awake."<< endl;
+                cout << "You run full speed at the figure."<< endl;
+                cout << "Right as you make contact with the figure, your body jolts awake."<< endl;
                 cout << "It was just a dream."<< endl;
                 cout << endl;
             }
@@ -42,34 +42,34 @@ int main()
             {
                 cout << "You run as quickly as you can out the door." << endl;
                 cout << "You hop in your car and drive."<< endl;
-                cout << "You as you try to leave your neighbourhood,"<< endl;
-                cout << "but you realize don't recognize any of the streets."<< endl;
+                cout << "You try to leave your neighbourhood, but you don't"<< endl;
+                cout << "recognize any of the streets."<< endl;
                 cout << "You drive and drive to no end."<< endl;
                 cout << "The houses lining the street seem to go on endlessly."<< endl;
-                cout << "The houses look less and less livable, not rundown,"<< endl;
-                cout << "but built as if there not made to be lived in."<< endl;
-                cout << "Houses with no windows, some with driveways that don't"<< endl;
-                cout << "lead to street instead they just circle the house,"<< endl;
-                cout << "others with entraces at the second floor of the house."<< endl;
-                cout << "You continue driving until you see a house with it's"<< endl;
-                cout << "light on."<< endl;
-                cout << "Cautiously, you walk in the house."<< endl;
-                cout << "The house looks exactily like yours."<< endl;
+                cout << "They begin to look less and less livable, not rundown,"<< endl;
+                cout << "but built as if they're not made to be lived in."<< endl;
+                cout << "Houses with no windows, some with driveways that just"<< endl;
+                cout << "circle the house and don't lead to the street, others"<< endl;
+                cout << "with entrances that are impractical to access."<< endl;
+                cout << "You continue driving until you see a house with its"<< endl;
+                cout << "lights on."<< endl;
+                cout << "You park and cautiously walk in the house."<< endl;
+                cout << "The house looks exactly like yours."<< endl;
                 cout << "You search the house but see no one."<< endl;
-                cout << "As you stand at the end of the hallway you hear the"<< endl;
+                cout << "As you stand at the end of the hallway, you hear the"<< endl;
                 cout << "phone ring."<< endl;
-                cout << "You see a some that looks identical to you walk to"<< endl;
-                cout << "and pick up the phone."<< endl;
+                cout << "You see someone that looks identical to youself walk"<< endl;
+                cout << "to the phone and pick it up."<< endl;
                 cout << "They turn to look at you..."<< endl;
-                cout << "Their face looks scared and hepless."<< endl;
-                cout << "Before you can say anything they run out the door."<< endl;
+                cout << "Their face looks scared and helpless."<< endl;
+                cout << "Before you can say anything, they run out the door."<< endl;
                 cout << endl;
             }
             else 
             {
                 cout << "You stand frozen."<< endl;
-                cout << "The figure approaches you and before you can react" << endl;
-                cout << "you succumb to the darkness."<< endl;
+                cout << "The figure approaches you, and before you can react..." << endl;
+                cout << "You succumb to the darkness."<< endl;
                 cout << endl;
             }
 
@@ -79,26 +79,26 @@ int main()
         {
             cout << "You wait for the ringing to stop." << endl;
             cout << "It doesn't."<< endl;
-            cout << "It gets louder"<< endl;
+            cout << "It gets louder."<< endl;
             cout << "The tones of the ring start merging together"<< endl;
             cout << "destorting, oscillating, and pulsing."<< endl;
-            cout << "AS the noise grows loader you sudenly feel weak."<< endl;
-            cout << "You collasps on the floor and use your last bit of "<< endl;
+            cout << "As the noise grows louder, you suddenly feel weak."<< endl;
+            cout << "You collapse on the floor and use your last bit of "<< endl;
             cout << "energy to dissconnect the phone from the wall."<< endl;
             cout << "The sound rings louder than before as one constant"<< endl;
             cout << "tone."<< endl;
             cout << "You blink and turn to the phone."<< endl;
             cout << "It's gone."<< endl;
-            cout << "Everthing is gone."<< endl;
-            cout << "The sound was now coming from the a heart monitor"<< endl;
+            cout << "Everything is gone."<< endl;
+            cout << "The sound was now coming from a heart monitor"<< endl;
             cout << "The white sheets of the hospital bed soak red."<< endl;
             cout << "The surgical team frantically tries to stop the"<< endl;
             cout << "bleeding."<< endl;
-            cout << "You close you eyes."<< endl;
+            cout << "You close your eyes."<< endl;
             
         }
     
-    cout << "Game Over"<< endl;
+    cout << "Game Over."<< endl;
     cout << endl;
     cout << endl;
 
