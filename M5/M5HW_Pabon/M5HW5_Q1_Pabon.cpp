378b6a25ce7e1a65ea0d3188ba8a4f7de5dd780c
FTCCEli <elimaximus11@gmail.com>
2025-04-20 03:27:37 +0000

    Q1 in process

---
+++ M5/M5HW_Pabon/M5HW5_Q1_Pabon.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// M5LAB1
+// Elijah Pabon
+// 4/13/25
+ 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{ 
+    string  month;
+    double inches;
+    for (int i = 0; i < 3; i++){
+        cout << "Enter month: ";
+        cin >> month;
+        cout << "Enter inches of rain: ";
+        cin >> inches;
+    }
+return 0;
+}
