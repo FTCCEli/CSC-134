396beebc9b11fc7eb987e5c86138a8d7009f63ec
FTCCEli <elimaximus11@gmail.com>
2025-04-20 18:38:49 +0000

    jo

---
+++ M5/M5HW_Pabon/M5HW_Q2_Pabon.cpp
@@ -0,0 +1,36 @@
+// CSC 134
+// M5HW
+// Elijah Pabon
+// 4/20/25
+ 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+double block_volume(double lenght, double width, double height){
+    double volume = lenght * width * height;
+    return volume;
+}
+int main()
+{
+    cout << "Question 2" << endl; 
+   double length = 0;
+   double width = 0;
+   double height = 0;
+   
+   while(length <= 0){
+    cout << "Enter length:";
+    cin >> length;
+   }
+   while(width <= 0){
+    cout << "Enter width:";
+    cin >> width;
+    }
+    while(height <= 0){
+        cout << "Enter height:";
+        cin >> height;
+        }
+    cout << "The volume of your hyperrectagle is: " << block_volume(length, width, height) << endl;
+    return 0;
+    
+   }
